24d4c203340b753bf3f8d7e05d3d394125fd0724
Khitishgithub <126503968+Khitishgithub@users.noreply.github.com>
2023-09-10 02:02:48 +0530

    added navbar

---
+++ WEB/client/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Navbar from "./Components/Navbar"
+import Body from './body/body'
+// import Footer from'./footer/footer'
+
+
+
+function App() {
+	return (
+		<React.Fragment>
+			<Navbar/>
+			<Body/>
+			{/* <Footer/> */}
+			
+		</React.Fragment>
+		
+	);
+}
+
+
+export default App;
