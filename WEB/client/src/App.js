5c65511c819442de15ffdb55d0f26219fb055b14
Goutam-04 <goutamnayak+github1@gmail.com>
2023-09-10 21:16:59 +0530

    fix bug of footer

---
+++ WEB/client/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';  
+import Navbar from "./Components/Navbar/Navbar"
+import Body from './Components/body/body'
+import Footer from './Components/footer/footer'
+
+
+
+function App() {
+	return (
+		<React.Fragment>
+			<Navbar/>
+			<Body/>
+			<Footer/>
+			
+		</React.Fragment>
+		
+	);
+}
+
+
+export default App;
